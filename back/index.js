a1b748d60472b79f48ba78361d449290d7adcd3f
Danils-sd <hlebushek.sd@gmail.com>
2024-06-11 04:23:34 +0300

    123

--- back/index.js
+++ back/index.js
@@ -10,62 +10,6 @@ app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
 app.use("/api", router);
-// const DBNike = require('./DataB/DBNike.json');
-// const DBAsics = require('./DataB/DBAsics.json');
-// const DBAdidas = require('./DataB/DBAdidas.json');
-
-// const DBMikasa = require('./DataB/DBMikasa.json');
-// const DBMolten = require('./DataB/DBMolten.json');
-
-// const DBZak = require("./DataB/DBZak.json");
-
-// app.get("/api/sneakers/nike", (req, res) => {
-//     res.json({data: DBNike});
-// })
-
-// app.get("/api/sneakers/asics", (req, res) => {
-//     res.json({data: DBAsics});
-// })
-
-// app.get("/api/sneakers/adidas", (req, res) => {
-//     res.json({data: DBAdidas});
-// })
-
-
-// app.get("/api/balls/mikasa", (req, res) => {
-//     res.json({data: DBMikasa});
-// })
-
-// app.get("/api/balls/molten", (req, res) => {
-//     res.json({data: DBMolten});
-// })
-
-// app.get("/api/admin-auth/:login/:password", (req, res) => {
-//     const json1 = fs.readFileSync("./DataB/DBAdmin.json", "utf8");
-//     const object = JSON.parse(json1);
-//     if((object.login === req.params["login"]) && (object.password === req.params["password"])){
-//         res.json({data: {
-//             res: true,
-//             content: DBZak
-//         }});
-//     } else {
-//         res.json({data: {
-//             res: false
-//         }});
-//     }
-// })
-
-// app.post("/api/zak", (req, res) => {
-//     const json1 = fs.readFileSync("./DataB/DBZak.json", "utf8")
-//     const object = JSON.parse(json1);
-//     object.id = Number(object.id) + 1;
-//     req.body.id = object.id;
-//     object.stuf.push(req.body);
-//     console.log(object);
-//     fs.writeFileSync("./DataB/DBZak.json", JSON.stringify(object, null, "\t"));
-//     console.log(fs.readFileSync("./DataB/DBZak.json", "utf8"));
-//     res.send({body: object.id});
-// })
 
 app.listen(PORT, () => {
     console.log(`http://localhost:${PORT}`);
